b81de3ea805402bd4ec4587915a2fa8bba067614
migueljarquin686 <114965097+migueljarquin686@users.noreply.github.com>
2022-11-21 20:51:39 -0600

    Update Dos numeros.cs

--- Dos numeros.cs
+++ Dos numeros.cs
@@ -1,6 +1,4 @@
-// Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
-// Es posible que el codigo generado no sea completamente correcto. Si encuentra
-// errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
 
 using System;
 
